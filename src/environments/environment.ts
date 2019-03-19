9ea1b6da72035b360187c2a6fc7c42300c9a6135
Vanderlei Silva <vanderlei@gmail.com>
2019-03-19 10:29:00 -0300

    Ajustes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://controle-financeiro-pessoal.herokuapp.com',
   bucketApiUrl: 'https://s3-sa-east-1.amazonaws.com/controle-financeiro-pessoal/',
   photoPrefix: 'up_',
   defaultTitle: 'Controle Financeiro - By Vanderlei',
