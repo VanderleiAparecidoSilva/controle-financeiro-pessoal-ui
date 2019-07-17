032cf9b05c6ab8428c02d530308bccb8a89102e3
Vanderlei Silva <vanderlei@gmail.com>
2019-07-17 16:10:31 -0300

    Adicionado envName no environment de producao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  envName: 'prod',
   apiUrl: 'https://controle-financeiro-pessoal.herokuapp.com',
   bucketApiUrl: 'https://s3-sa-east-1.amazonaws.com/controle-financeiro-pessoal/',
   photoPrefix: 'up_',
